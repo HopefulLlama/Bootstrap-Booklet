6bec7d4a2894c4b0bae69ef9147c2656f70bc547
HopefulLlama <jonlaw10170@gmail.com>
2014-11-04 12:25:29 +0000

    - Better examples - Updated README.md

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ Installation
   a. jQuery <br />
   b. Bootstrap <br />
 If not, please download the latest tested version from <a href="https://github.com/HopefulLlama/Bootstrap-Booklet/tree/master/lib">Bootstrap-Booklet/lib</a>.
-2. Download the minified version from <a href="https://raw.githubusercontent.com/HopefulLlama/Bootstrap-Booklet/master/bin/bootstrap-booklet-0.1.min.js">here</a>.
+2. Download the minified version from <a href="https://raw.githubusercontent.com/HopefulLlama/Bootstrap-Booklet/master/bin/bootstrap-booklet-0.3.min.js">here</a>.
 3. Import all files into the relevant HTML page.
+
+Live Example
+------------
+
+To view a live, working example with several customisation options, click <a href="http://hopefulllama-portfolio.herokuapp.com/portfolio/Bootstrap-Booklet/example.html">here</a>.
 
 Quick Start
 -----------
--- js/example.js
+++ js/example.js
@@ -20,15 +20,17 @@ function setUpHelp() {
 	});
 
 	var booklet = new BootstrapBooklet("#booklet");
-	booklet.addPage("This is the default booklet", "<p>Some test data</p>");
-	booklet.addPage("with no customisation of styling or options", "<p>Some more test data</p>");
+	booklet.addPage("This is the Default Booklet", "<p>This booklet has no customisation options or styling applied to it.</p>");
+	booklet.addPage("Initialising the Booklet", '<p>First, in the HTML, create a div(s) with a selector to be used. This will act as a placeholder for the booklet to be created in. Then create a BootstrapBooklet object, passing in a jQuery style selector. For example, to create a booklet on a div with ID "booklet":</p><p>var booklet = new BootstrapBooklet("#booklet");</p>');
+	booklet.addPage("Adding Pages", '<p>After initialising the booklet, you can begin adding some pages by using the addPage function. This requires the page title, and body (in HTML) as parameters as the following example shows:</p><p>booklet.addPage("title", "HTML body");</p>');
+	booklet.addPage("Generating a Booklet", '<p>Once your booklet is finished, you must generate it to display the booklet itself. This is done as such:</p><p>booklet.generate();</p>');
 	booklet.generate();
 
 
 	var booklet2 = new BootstrapBooklet("#booklet2");
-	booklet2.addPage("This booklet", "<p>De ja vu</p>");
-	booklet2.addPage("has different styling", "<p>Rendezvous</p>");
-	booklet2.addPage("and no page numbers on contents", "<p>Et tu?</p>");
+	booklet2.addPage("Customisations!", "<p>This booklet has different styling an no page numbers.</p>");
+	booklet2.addPage("Changing panelClasses", '<p>You can change the styling by applying different classes to the booklet, then applying the CSS styling to that class. This can be done with the following code snippet: </p><p>booklet2.panelClasses="panel-danger";</p>');
+	booklet2.addPage("Removing Page Numbers", "<p>The page number labels on the contents page can be disabled by altering the Contents object as such: </p><p>booklet2.contents.showPageNumbers = false;</p>");
 
 	booklet2.panelClasses = "panel-danger";
 
@@ -38,9 +40,8 @@ function setUpHelp() {
 
 
 	var booklet2 = new BootstrapBooklet("#booklet3");
-	booklet2.addPage("Look! No Contents page", "<p>123</p>");
-	booklet2.addPage("on this booklet.", "<p>ABC</p>");
-	booklet2.addPage("Contents are stupid anyway.", "<p>Got to be</p>");
+	booklet2.addPage("Look! No Contents page", "<p>This booklet has completed disabled the contents page. By default, the first page of the booklet will be shown on load instead.</p>");
+	booklet2.addPage("Disabling the contents page", "<p>Contents can be disabled by using the following code snippet:</p><p>booklet2.contents.enabled = false;</p>");
 
 	booklet2.panelClasses = "panel-success";
 
